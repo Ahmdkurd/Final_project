8c5d3ebf621250569c25c66c7494b617d55f19de
Ahmdkurd <107446041+Ahmdkurd@users.noreply.github.com>
2023-09-17 01:13:41 +0300

    TODO 2

--- final_project.py
+++ final_project.py
@@ -9,7 +9,13 @@ Delivery Date :17-9
 # course_id (generated using uuid4) ,
 # course name (user_input) and
 # course mark (user_input)
+import uuid
 
+class Course:
+    def __init__(self, course_name, course_mark):
+        self.course_id = str(uuid.uuid4())
+        self.course_name = course_name
+        self.course_mark = course_mark
 
 class Student:
     # TODO 3 define static variable indicates total student count
