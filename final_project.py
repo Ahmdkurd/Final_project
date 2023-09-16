1eb1e8780036c904db3cc2e000d1a87247b767a0
Ahmdkurd <107446041+Ahmdkurd@users.noreply.github.com>
2023-09-17 01:24:43 +0300

    TODO 4

--- final_project.py
+++ final_project.py
@@ -27,8 +27,13 @@ class Student:
     # student_age (user input)
     # student_number (user_input)
     # courses_list (List of Course Objects)
-    def __init__(self):
-        pass
+    def __init__(self, student_name, student_age, student_number):
+        self.student_id = str(uuid.uuid4())
+        self.student_name = student_name
+        self.student_age = student_age
+        self.student_number = student_number
+        self.courses_list = []
+        Student.total_students += 1
 
     # TODO 5 define a method to enroll new course to student courses list
 
